b896f673f755e6f27b5d24f5ab3a791b92eeb046
Mi <74912354+milehagen@users.noreply.github.com>
2021-10-04 13:38:32 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import WeatherSearch from './WeatherSearch';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <div>
+          <h1>Weather App</h1>
+            <WeatherSearch/>
+        </div>
+
+      </header>
+    </div>
+  );
+}
+
---
+++ src/WeatherSearch.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function WeatherSearch(){
+    <h1>Weather App</h1>
+    return <form> 
+        <input type="search"
+        autoFocus="on"
+        placeholder="Enter a city..."/>
+    <input type="submit"
+    value="Search"/>
+
+    </form>
+}
